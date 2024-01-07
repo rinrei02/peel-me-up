271e8435dbe75cb4588cd53998b7b91ad34940e5
riversans <155889310+rinrei02@users.noreply.github.com>
2024-01-07 22:00:13 +0800

    Minor updates

--- js/site.js
+++ js/site.js
@@ -1,17 +1,36 @@
-window.products = {
-    getAll: function () {
-
-		let url = './products.json';
-		let options = {
+window.products =
+{
+    getAll: function()
+	{
+		this.loadData((data) =>
+		{
+			console.log('ALL DATA LOADED');
+		})
+	},
+	getSelectedDetails: function() 
+	{
+		this.loadData((data) =>
+		{
+			console.log('SELECTED DETAILS LOADED');
+		})
+	},
+	loadData: function(callback)
+	{
+		let dataUrl = './data/products.json';
+		let options =
+		{
 			method: 'GET',
 			headers: { 'Content-Type': 'application/json' }
 		};
-
-		fetch(url, options)
+		fetch(dataUrl, options)
 			.then(response => response.json())
-			.then(data => console.log(data))
+			.then(data =>
+			{
+				console.log(data);
+				callback(data);
+			})
 			.catch(error => console.log(error));
-	}
+	},
 };
 
 
